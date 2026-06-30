bf533c66d02d289c1b85a2693efb7fdd3d299753
JC <576461713@qq.com>
2026-06-30 09:59:29 +0800

    Implement VLM element refiner

--- modules/vlm/__init__.py
+++ modules/vlm/__init__.py
@@ -2,5 +2,6 @@
 
 from .client import OpenAICompatibleVLMClient, VLMClientError, parse_json_object
 from .prompt_planner import VLMPromptPlanner
+from .element_refiner import VLMElementRefiner
 
-__all__ = ["OpenAICompatibleVLMClient", "VLMClientError", "parse_json_object", "VLMPromptPlanner"]
+__all__ = ["OpenAICompatibleVLMClient", "VLMClientError", "parse_json_object", "VLMPromptPlanner", "VLMElementRefiner"]
