24f86508a3fb9af001aaf78e15b5ff0d3854c5f9
JC <576461713@qq.com>
2026-06-30 09:54:49 +0800

    Add VLM prompt planner under VLM package

--- modules/vlm/__init__.py
+++ modules/vlm/__init__.py
@@ -1,5 +1,6 @@
 """Unified VLM client package."""
 
 from .client import OpenAICompatibleVLMClient, VLMClientError, parse_json_object
+from .prompt_planner import VLMPromptPlanner
 
-__all__ = ["OpenAICompatibleVLMClient", "VLMClientError", "parse_json_object"]
+__all__ = ["OpenAICompatibleVLMClient", "VLMClientError", "parse_json_object", "VLMPromptPlanner"]
