3e749bdca4c6c29f663301bd400f98fd24c54c37
JC <576461713@qq.com>
2026-06-30 10:25:04 +0800

    Implement VLM export validator module

--- modules/vlm/__init__.py
+++ modules/vlm/__init__.py
@@ -3,5 +3,6 @@
 from .client import OpenAICompatibleVLMClient, VLMClientError, parse_json_object
 from .prompt_planner import VLMPromptPlanner
 from .element_refiner import VLMElementRefiner
+from .export_validator import VLMExportValidator
 
-__all__ = ["OpenAICompatibleVLMClient", "VLMClientError", "parse_json_object", "VLMPromptPlanner", "VLMElementRefiner"]
+__all__ = ["OpenAICompatibleVLMClient", "VLMClientError", "parse_json_object", "VLMPromptPlanner", "VLMElementRefiner", "VLMExportValidator"]
